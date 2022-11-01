bf676cbfff5f8ec54a5baf6aa5ca87386dc85166
naiakpy <114989408+naiakpy@users.noreply.github.com>
2022-11-01 17:44:01 +0700

    Add files via upload

--- InterfaceAbstract/InterfaceAbstractNayya.java
+++ InterfaceAbstract/InterfaceAbstractNayya.java
@@ -8,7 +8,7 @@ package interfaceabstract;
  *
  * @author nayyakamila
  */
-public class InterfaceAbstractFerza {
+public class InterfaceAbstractNayya {
 
     /**
      * @param args the command line arguments
